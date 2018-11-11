2ee177c59a6662bd4e46c932153bfa9a8667f823
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-11 22:24:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
-  array.join(",")
+  if array.length == 1 
+    array.join(",")
   if array.length == 2 
     array.join(" and ")
+  if array.length >= 3
+    array.join
   end 
 end
